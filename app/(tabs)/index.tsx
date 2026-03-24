79f448b9be9fdcf3bcb9883d4f74d138e02c8ede
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2026-03-24 18:15:03 +0100

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,13 +8,12 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
     return (
         <SafeAreaView className="flex-1 bg-background p-5">
-            <Text className="text-3xl text-primary font-sans-extrabold">
-                Welcome to Recurrly!
-            </Text>
+            <Text className="text-5xl font-sans-extrabold">Home</Text>
 
-            <Link href="/onboarding" className="font-sans-bold mt-4 rounded bg-primary text-white p-4">Go to Onboarding</Link>
-            <Link href="/(auth)/sign-in" className=" font-sans-bold mt-4 rounded bg-primary text-white p-4">Go to Sign in</Link>
-            <Link href="/(auth)/sign-up" className=" font-sans-bold mt-4 rounded bg-primary text-white p-4">Go to Sign up</Link>
+            <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Onboarding</Link>
+            <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Sign in</Link>
+            <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Sign up</Link>
+
         </SafeAreaView>
     );
 }
